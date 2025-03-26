8c19d3cf1a99e866051fad3311e3c204553afc71
Ayoub Taouam <ayoubtaouam@gmail.com>
2025-03-26 17:42:40 +0000

    Change playlist name

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,10 +1,16 @@
-import { Card, ListGroup, Button, ListGroupItem } from "react-bootstrap";
+import { Card, ListGroup, Button, ListGroupItem, Form, FormControl } from "react-bootstrap";
 import PlaylistTrack from "../PlaylistTrack/PlaylistTrack";
 
 function Playlist({playlist, setPlaylist}) {
     return (
         <Card className="bg-success text-light bg-opacity-75">
-            <Card.Header className="fw-bolder fs-2">Playlist</Card.Header>
+            <Card.Header className="">
+                <Form>
+                    <FormControl type="text" 
+                                 placeholder="Playlist name..." 
+                                 className="bg-success bg-opacity-75 p-0 border border-0 text-light fw-bolder fs-2"></FormControl>
+                </Form>
+            </Card.Header>
             <ListGroup variant="flush">
                 {
                     playlist.map((t, i) => <PlaylistTrack key={i} track={t} setPlaylist={setPlaylist} />)
